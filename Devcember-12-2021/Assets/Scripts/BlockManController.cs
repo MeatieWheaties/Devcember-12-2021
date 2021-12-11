3e9baa11685fd28c13f4248a476b1e5aa4417494
Jordan <jordan.fest1@gmail.com>
2021-12-11 12:15:59 -0600

    Update BlockManController.cs
    Updated the controller to support jump

--- Devcember-12-2021/Assets/Scripts/BlockManController.cs
+++ Devcember-12-2021/Assets/Scripts/BlockManController.cs
@@ -1,21 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class BlockManController : MonoBehaviour
 {
     [SerializeField]private float speed; //how fast the character moves
     private float direction; // tells if the player goes left or right
-	private bool canJump; //verifies if the player can jump
+
+    #region Jump Properties
+
     [SerializeField]private float jumpHeight; //how high the player can jump
-	
+    private bool cmdJump; //Indicates a request to jump by player
+    private bool canJump; //Can block a player from holding down spacebar to autojump
+    private bool isGrounded;
+
+    #endregion
+
+    #region Meta Properties
+
+    private new BoxCollider2D collider => transform.GetComponent<BoxCollider2D>();
+
+    #endregion
+
     private Rigidbody2D rBody;
 	
 	
 	void Start()
 	{
 		rBody = gameObject.GetComponent<Rigidbody2D>(); //get the rigidbody
-	}
+    }
 	
 	void Update()
     {
@@ -34,27 +51,87 @@ public class BlockManController : MonoBehaviour
 		
 		if(Input.GetKey(KeyCode.Space))
 		{
-			canJump = true;
+            cmdJump = true;
 		}
-		else
-		{
-			canJump = false;
-		}
+        else
+        {
+            cmdJump = false;
+        }
+
+        UpdateIsGrounded();
+        UpdateCanJump();
     }
 	
 	void FixedUpdate()
 	{
 		rBody.velocity = (transform.right * direction * speed * Time.fixedDeltaTime); //left and right movement
-		
-		Debug.DrawRay(new Vector2(transform.position.x, transform.position.y - 0.065f), -Vector2.up, Color.red);
-		
-		if(canJump)
-		{
-			RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.066f), -Vector2.up * 0.001f);
-			if(hit)
-			{
-				rBody.AddForce(transform.up * jumpHeight * Time.fixedDeltaTime, ForceMode2D.Impulse);
-			}
-		}
+
+        if(canJump && cmdJump)
+        {
+            Jump();
+        }
 	}
+
+    #region Commands
+
+    private void Jump()
+    {
+        rBody.AddForce(transform.up * jumpHeight * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        canJump = false;
+    }
+
+    #endregion
+
+    #region Updates
+
+    /// <summary>
+    /// Used to confirm if the player is grounded and they have at least release the jump command before command a jump again
+    /// </summary>
+    private void UpdateCanJump()
+    {
+        if (!canJump && isGrounded && !cmdJump) //Player let go of jump in order for this to happen
+        {
+            canJump = true;
+        } else if (!isGrounded)
+        {
+            canJump = false;
+        }
+    }
+
+    private void UpdateIsGrounded()
+    {
+        var hit = Physics2D.Raycast(transform.position, -Vector2.up,collider.size.y / 1.25f/*,  2 Ignore Raycast*/);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    #endregion
+
+    #region Debug
+
+    void OnDrawGizmos()
+    {
+        //DrawJumpRay();
+        //DrawIsGrounded();
+    }
+
+    private void DrawJumpRay()
+    {
+        Gizmos.color = Color.yellow;
+        Debug.DrawRay(transform.position, -Vector2.up * collider.size.y / 1.25f, Color.red);
+    }
+
+    private void DrawIsGrounded()
+    {
+        Gizmos.color = isGrounded ? Color.green : Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.up);
+    }
+
+    #endregion
 }
